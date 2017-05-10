e01623e927e3a6cebf63043311b69042a593881f
David Vezzani <david_vezzani@yahoo.com>
2017-05-10 16:40:07 -0700

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/drag', function(req, res){
+res.sendFile(__dirname + '/drag.html');
+});
+
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/index.html');
+});
+
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });  
+// });
+
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     console.log('message: ' + msg);
+//   });
+// });
+
+var users = [];
+  
+io.on('connection', function(socket){
+  socket.on('login', function(username){
+    socket.username = username;
+    users.push(username);
+    io.emit('chat message', socket.username + " has joined");
+    console.log('user registered');
+  });
+
+  socket.on('chat message', function(msg){
+    socket.broadcast.emit('chat message', socket.username + ": " + msg);
+    // io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });  
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
